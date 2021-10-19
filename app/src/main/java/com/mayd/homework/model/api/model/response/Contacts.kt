03988d2ab0dd7764a0615c7e35627cd5c65bcba8
Neo <hu.yu.huei@gmail.com>
2021-10-19 18:46:18 +0800

    clean

--- app/src/main/java/com/mayd/homework/model/api/model/response/Contacts.kt
+++
@@ -1,9 +0,0 @@
-package com.mayd.homework.model.api.model.response
-
-
-import com.google.gson.annotations.SerializedName
-
-data class Contacts(
-    @SerializedName("contacts")
-    val contacts: ArrayList<Contact>
-)
